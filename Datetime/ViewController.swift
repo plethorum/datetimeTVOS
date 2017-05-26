76716a6da80c97d71a6099599af384a104ec41ff
plethorum <samyar.vahid@gmail.com>
2017-05-25 21:59:31 -0400

    did tutorial on tvos

--- Datetime/ViewController.swift
+++ Datetime/ViewController.swift
@@ -13,14 +13,37 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        print("Hello, World!")
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+   
+    @IBAction func button0tapped(_ sender: Any) {
+        showAlert(status: "Wrong!", title: "Bummer, you got it wrong!")
+    }
+    @IBAction func button1tapped(_ sender: Any) {
+        showAlert(status: "Correct!", title: "Whoo! That is the correct response")
+    }
+    @IBAction func button2tapped(_ sender: Any) {
+        showAlert(status: "Wrong!", title: "Bummer, you got it wrong!")
+    }
+    @IBAction func button3tapped(_ sender: Any) {
+        showAlert(status: "Wrong!", title: "Bummer, you got it wrong!")
+    }
+    func showAlert(status: String, title:String) {
+        let alertController = UIAlertController(title: status, message: title, preferredStyle: .alert)
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel) { (action) in
+        }
+        alertController.addAction(cancelAction)
+        let ok = UIAlertAction(title: "OK", style: .default) { (action) in
+        }
+        alertController.addAction(ok)
+        
+        self.present(alertController, animated: true) {
+        }
+    }
+    }
 
-
-}
 
