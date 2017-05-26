16cb105a652edf7c9cf1920c5849eb548769b623
plethorum <samyar.vahid@gmail.com>
2017-05-25 23:20:55 -0400

    tbh idk what im doing

--- Datetime/ViewController.swift
+++ Datetime/ViewController.swift
@@ -9,7 +9,6 @@
 import UIKit
 
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,31 +18,23 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-   
-    @IBAction func button0tapped(_ sender: Any) {
-        showAlert(status: "Wrong!", title: "Bummer, you got it wrong!")
-    }
-    @IBAction func button1tapped(_ sender: Any) {
-        showAlert(status: "Correct!", title: "Whoo! That is the correct response")
-    }
-    @IBAction func button2tapped(_ sender: Any) {
-        showAlert(status: "Wrong!", title: "Bummer, you got it wrong!")
-    }
-    @IBAction func button3tapped(_ sender: Any) {
-        showAlert(status: "Wrong!", title: "Bummer, you got it wrong!")
-    }
-    func showAlert(status: String, title:String) {
-        let alertController = UIAlertController(title: status, message: title, preferredStyle: .alert)
-        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel) { (action) in
+    var gradientLayer: CAGradientLayer!
+    func createGradientLayer() {
+        gradientLayer = CAGradientLayer()
+            
+        gradientLayer.frame = self.view.bounds
+            
+        gradientLayer.colors = [UIColor.red.cgColor, UIColor.yellow.cgColor]
+            
+        self.view.layer.addSublayer(gradientLayer)
         }
-        alertController.addAction(cancelAction)
-        let ok = UIAlertAction(title: "OK", style: .default) { (action) in
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        createGradientLayer()
         }
-        alertController.addAction(ok)
-        
-        self.present(alertController, animated: true) {
-        }
-    }
-    }
-
-
+}
+func angeryFace() {
+    let layer = CAShapeLayer()
+    layer.path = UIBezierPath(roundedRect: CGRect(x: 64, y: 64, width: 160, height: 160), cornerRadius: 50).cgPath
+    layer.fillColor = UIColor.green.cgColor
+    view.layer.addSublayer(layer)}
